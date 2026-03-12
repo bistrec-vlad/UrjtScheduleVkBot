e42e6f8549a0a24f31acd594e9f9eb14e6f5383a
bistrec-vlad <vlakiy972@gmail.com>
2026-03-12 17:22:03 +0500

    rename findNeedingRenewalSubscriptions

---
+++ public/api/sendMessageForNeedingRenewalSubscriptions.php
@@ -0,0 +1,77 @@
+<?php
+require_once __DIR__ . "/../../config/vkApi.php";
+require_once __DIR__ . "/../../config/database.php";
+
+require_once __DIR__ . "/../vendor/autoload.php";
+
+require_once __DIR__ . "/../apiClients/VkBotApiClient.php";
+require_once __DIR__ . "/../repositories/VkBotRepository.php";
+require_once __DIR__ . "/../repositories/SqlUserRepository.php";
+require_once __DIR__ . "/../repositories/SqlSubscriptionRepository.php";
+require_once __DIR__ . "/../repositories/SqlOrderRepository.php";
+require_once __DIR__ . "/../repositories/SqlLogRepository.php";
+require_once __DIR__ . "/../repositories/SqlScheduleFileRepository.php";
+require_once __DIR__ . "/../Logger.php";
+
+use VK\Client\VKApiClient;
+
+$vkApiClient = new VKApiClient(API_VERSION);
+$botApiClient = new VkBotApiClient($vkApiClient, TOKEN);
+
+$pdo = new PDO(SQL_DSN, SQL_USERNAME, SQL_PASSWORD);
+$botRepo = new VkBotRepository(
+    new SqlUserRepository($pdo, SQL_USERS_TABLE_NAME),
+    new SqlSubscriptionRepository($pdo, SQL_SUBSCRIPTIONS_TABLE_NAME),
+    new SqlOrderRepository($pdo, SQL_ORDERS_TABLE_NAME),
+    new SqlLogRepository($pdo, SQL_LOGS_TABLE_NAME),
+    new SqlScheduleFileRepository($pdo, SQL_SCHEDULE_FILES_TABLE_NAME),
+);
+
+$userRepo = $botRepo->getUserRepository();
+$subscriptionRepo = $botRepo->getSubscriptionRepository();
+
+$logger = new Logger($botRepo->getLogRepository());
+
+$needingRenewalSubscriptions = $subscriptionRepo->findAllNeedingRenewal();
+
+if (!isset($needingRenewalSubscriptions)) {
+    exit(0);
+}
+
+$keyboard = [
+    "inline" => true,
+    "buttons" => [
+        [
+            [
+                "action" => [
+                    "type" => "callback",
+                    "payload" => json_encode(
+                        ["button" => "moveToPayment"],
+                        JSON_UNESCAPED_UNICODE,
+                    ),
+                    "label" => "➡️ Перейти к оплате",
+                ],
+                "color" => "primary",
+            ],
+        ],
+    ],
+];
+
+foreach ($needingRenewalSubscriptions as $sub) {
+    if ($sub->isNoneType() || $sub->isUnlimitedType()) {
+        continue;
+    }
+
+    $user = $userRepo->findById($sub->getUserId());
+
+    $botApiClient->sendKeyboardMessage(
+        $user->getChatId(),
+        "Нужно оплатить подписку чел",
+        json_encode($keyboard),
+        3,
+    );
+
+    $subscriptionRepo->updateType($sub->getId(), "none");
+}
+
+echo "ok";
